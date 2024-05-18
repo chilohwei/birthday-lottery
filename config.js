401e7eed99e2d265f5d2e4e585bdc08d3a3cac6e
chilohwei <chilohwei@gmail.com>
2024-05-18 17:14:37 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+    introMessages: [
+        `今天是农历四月十号，祝你生日快乐~`,
+        `关于生日礼物<br/>我想了很多方案<br/>都被我一一否决了...`,
+        `最后，我选择了一个不一样的方案：<br/><span class='highlight'>生日大抽奖</span><br/>由你自己来选择幸运礼物`
+    ],
+    emojiList: ['🎂', '🎉', '🎁', '🎈', '🍰'],
+    contactPerson: '魏奇洛',
+    prizes: {
+        '奖项1': { description: '影视会员年卡 1 张', icon: '🎬' },
+        '奖项2': { description: '任意电影票 3 张', icon: '🎟️' },
+        '奖项3': { description: '生日红包 88 元', icon: '💸' },
+        '奖项4': { description: '海马体艺术照 1 组', icon: '📸' },
+        '奖项5': { description: '帮实现 1 个愿望', icon: '🌠' },
+        '奖项6': { description: '增加 1 次抽奖机会', icon: '🔁' }
+    }
+};
