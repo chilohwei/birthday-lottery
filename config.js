633461838956ad2c082a34a1d724a42d7df0cb78
chilohwei <chilohwei@gmail.com>
2025-04-17 18:07:20 +0800

    style: 统一色彩方案并优化UI设计

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
     introMessages: [
-        `今天是农历四月十号，祝你节日快乐~`, //第一句话
-        `关于节日礼物<br/>我想了很多方案<br/>都被我一一否决了...`, //第二句话
-        `最后，我选择了一个不一样的方案：<br/><span class='highlight'>生日大抽奖</span><br/>由你自己来选择幸运礼物` //第三句话
+        `今天是农历四月二十号，祝佳佳节日快乐~`, //第一句话
+        `关于节日礼物，我想<br/>老王就是你最好的礼物<br/>...`, //第二句话
+        `最后，我想换种方式：<br/><span class='highlight'>生日大抽奖</span><br/>由你自己来选择幸运礼物` //第三句话
     ],
     emojiList: ['🎂', '🎉', '🎁', '🎈', '🍰', '🧧'], //生日特效的符号
-    contactPerson: '魏奇洛', //兑奖人的姓名，比如你自己
+    contactPerson: '老魏', //兑奖人的姓名，比如你自己
     prizes: {
         '奖项1': { description: '影视会员年卡 1 张', icon: '🎬' },
         '奖项2': { description: '任意电影票 3 张', icon: '🎟️' },
-        '奖项3': { description: '生日红包 88 元', icon: '💸' },
-        '奖项4': { description: '海马体艺术照 1 组', icon: '📸' },
+        '奖项3': { description: '生日红包 188 元', icon: '💸' },
+        '奖项4': { description: '结婚礼金+1000元', icon: '📸' },
         '奖项5': { description: '帮实现 1 个愿望', icon: '🌠' },
         '奖项6': { description: '增加 1 次抽奖机会', icon: '🔁' }
     }
